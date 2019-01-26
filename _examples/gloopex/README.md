b2a74596bc22468422ea05719083f1f195677a91
erinpentecost <erin@pentecost.email>
2019-01-25 17:35:25 -0800

    consolidating example repo into this one

---
+++ _examples/gloopex/README.md
@@ -0,0 +1,5 @@
+# gloopex
+
+Golang game loop example using [gloop](https://github.com/erinpentecost/gloop). This is a modification of the [vulkan-go/demos](https://github.com/vulkan-go/demos) cube demo.
+
+When running the demo, go to [localhost:8000](http://localhost:8000/) to see metrics published by gloop.
---
+++ _examples/gloopex/example.go
@@ -0,0 +1,173 @@
+package main
+
+import (
+	"log"
+	"runtime"
+	"time"
+
+	"github.com/erinpentecost/gloop"
+	as "github.com/vulkan-go/asche"
+	"github.com/vulkan-go/demos/vulkancube"
+	"github.com/vulkan-go/glfw/v3.3/glfw"
+	vk "github.com/vulkan-go/vulkan"
+	"github.com/xlab/closer"
+)
+
+func init() {
+	runtime.LockOSThread()
+	log.SetFlags(log.Lshortfile)
+}
+
+type Application struct {
+	*vulkancube.SpinningCube
+	debugEnabled bool
+	windowHandle *glfw.Window
+}
+
+func (a *Application) VulkanSurface(instance vk.Instance) (surface vk.Surface) {
+	surfPtr, err := a.windowHandle.CreateWindowSurface(instance, nil)
+	if err != nil {
+		log.Println(err)
+		return vk.NullSurface
+	}
+	return vk.SurfaceFromPointer(surfPtr)
+}
+
+func (a *Application) VulkanAppName() string {
+	return "VulkanCube"
+}
+
+func (a *Application) VulkanLayers() []string {
+	return []string{
+		// "VK_LAYER_GOOGLE_threading",
+		// "VK_LAYER_LUNARG_parameter_validation",
+		// "VK_LAYER_LUNARG_object_tracker",
+		// "VK_LAYER_LUNARG_core_validation",
+		// "VK_LAYER_LUNARG_api_dump",
+		// "VK_LAYER_LUNARG_swapchain",
+		// "VK_LAYER_GOOGLE_unique_objects",
+	}
+}
+
+func (a *Application) VulkanDebug() bool {
+	return false // a.debugEnabled
+}
+
+func (a *Application) VulkanDeviceExtensions() []string {
+	return []string{
+		"VK_KHR_swapchain",
+	}
+}
+
+func (a *Application) VulkanSwapchainDimensions() *as.SwapchainDimensions {
+	return &as.SwapchainDimensions{
+		Width: 500, Height: 500, Format: vk.FormatB8g8r8a8Unorm,
+	}
+}
+
+func (a *Application) VulkanInstanceExtensions() []string {
+	extensions := a.windowHandle.GetRequiredInstanceExtensions()
+	if a.debugEnabled {
+		extensions = append(extensions, "VK_EXT_debug_report")
+	}
+	return extensions
+}
+
+func NewApplication(debugEnabled bool) *Application {
+	return &Application{
+		SpinningCube: vulkancube.NewSpinningCube(1.0),
+
+		debugEnabled: debugEnabled,
+	}
+}
+
+func main() {
+	orPanic(glfw.Init())
+	vk.SetGetInstanceProcAddr(glfw.GetVulkanGetInstanceProcAddress())
+	orPanic(vk.Init())
+	defer closer.Close()
+
+	app := NewApplication(true)
+	reqDim := app.VulkanSwapchainDimensions()
+	glfw.WindowHint(glfw.ClientAPI, glfw.NoAPI)
+	window, err := glfw.CreateWindow(int(reqDim.Width), int(reqDim.Height), "VulkanCube (GLFW and gloop)", nil, nil)
+	orPanic(err)
+	app.windowHandle = window
+
+	// creates a new platform, also initializes Vulkan context in the app
+	platform, err := as.NewPlatform(app)
+	orPanic(err)
+
+	dim := app.Context().SwapchainDimensions()
+	log.Printf("Initialized %s with %+v swapchain", app.VulkanAppName(), dim)
+
+	loop, _ := gloop.NewLoop(nil, nil, gloop.Hz60Delay, gloop.Hz60Delay)
+
+	render := func(step time.Duration) error {
+		imageIdx, outdated, err := app.Context().AcquireNextImage()
+		if err != nil {
+			return err
+		}
+		// outdated is true if the window changed shape or something,
+		// indicating that it should be redrawn.
+		if outdated {
+			imageIdx, _, err = app.Context().AcquireNextImage()
+			if err != nil {
+				return err
+			}
+		}
+		_, err = app.Context().PresentImage(imageIdx)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+
+	simulate := func(step time.Duration) error {
+		if window.ShouldClose() {
+			loop.Stop(nil)
+			return nil
+		}
+		glfw.PollEvents()
+		app.NextFrame()
+		return nil
+	}
+
+	loop.Render = render
+	loop.Simulate = simulate
+
+	metrics := NewMetricsServer()
+	metrics.Serve(loop.Done())
+
+	loop.Start()
+
+	for {
+		select {
+		case <-loop.Done():
+			app.Destroy()
+			platform.Destroy()
+			window.Destroy()
+			glfw.Terminate()
+			break
+		case s := <-loop.Heartbeat():
+			metrics.Publish(s)
+		}
+	}
+}
+
+func orPanic(err interface{}) {
+	switch v := err.(type) {
+	case error:
+		if v != nil {
+			panic(err)
+		}
+	case vk.Result:
+		if err := vk.Error(v); err != nil {
+			panic(err)
+		}
+	case bool:
+		if !v {
+			panic("condition failed: != true")
+		}
+	}
+}
